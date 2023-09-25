5afd4f1f5dae44f56d3c9317329c1852267702f8
k3v-5 <kevingarrido711@gmail.com>
2023-09-25 07:28:13 -0600

    Comit 25-09

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,23 +1,27 @@
+using API.Controllers;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController : ControllerBase
+[Authorize]
+public class UsersController : BaseApiController
 {
     private readonly DataContext _context;
     public UsersController(DataContext context)
     {
         _context = context;
     }
+
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUSer>>> GetUsers()
     {
         return await _context.Users.ToListAsync();
     }
 
+    [Authorize]
     [HttpGet("{id}")]
     public async Task<ActionResult<AppUSer>>GetUser (int id)
     {
---
+++ API/DTOS/UserDto.cs
@@ -0,0 +1,11 @@
+
+namespace API.DTOS
+{
+    public class UserDto
+    {
+
+     public string Username {get; set;}
+
+     public string Token {get; set;}   
+    }
+}
