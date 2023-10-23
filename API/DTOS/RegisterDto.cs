8d10379e3419612b214cc261f0b4316b4ec84565
k3v-5 <kevingarrido711@gmail.com>
2023-10-23 07:11:57 -0600

    Seccion 07

--- API/DTOS/RegisterDto.cs
+++ API/DTOS/RegisterDto.cs
@@ -6,7 +6,9 @@ namespace API.DTOS
     {
         [Required]
         public string UserName {get; set;}
+
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string password {get; set;}
 
 
