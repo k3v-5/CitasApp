9ad45f046fbbae9495224b9c37b1ef3dbf6744eb
k3v-5 <142853881+k3v-5@users.noreply.github.com>
2024-04-03 07:47:52 -0600

    26/03/24

--- SPA/src/app/members/member-list/member-list.component.ts
+++ SPA/src/app/members/member-list/member-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { Observable } from "rxjs";
 import { IMember } from "src/app/_models/imember";
+import { Pagination } from "src/app/_models/pagination";
 import { MembersService } from "src/app/_services/members.service";
 
 @Component({
@@ -9,11 +10,32 @@ import { MembersService } from "src/app/_services/members.service";
   styleUrls: ["./member-list.component.css"],
 })
 export class MemberListComponent {
-  members$: Observable<IMember[]> | undefined;
+  members: IMember[] = [];
+  pagination: Pagination | undefined;
+  pageNumber = 1;
+  pageSize = 5;
 
   constructor(private membersService: MembersService) {}
 
   ngOnInit(): void {
-    this.members$ = this.membersService.getMembers();
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.membersService.getMembers(this.pageNumber, this.pageSize).subscribe({
+      next: (response) => {
+        if (response.result && response.pagination) {
+          this.members = response.result;
+          this.pagination = response.pagination;
+        }
+      },
+    });
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMembers();
+    }
   }
 }
