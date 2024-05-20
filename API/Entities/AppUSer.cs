4d9e7516bbe57575f4cf9132146b44e954030098
k3v-5 <142853881+k3v-5@users.noreply.github.com>
2024-05-20 17:23:52 -0600

    Parcial03 - Added likes entity

--- API/Entities/AppUSer.cs
+++ API/Entities/AppUSer.cs
@@ -30,8 +30,10 @@ namespace API.Entities
 
         public string Country {get; set;}
 
-        public List<Photo> Photos {get; set;} = new ();
+        public List<Photo> Photos { get; set; } = [];
 
+    public List<UserLike> LikedByUsers { get; set; }
+    public List<UserLike> LikedUsers { get; set; }
         //public int DameLaEdad(){
         //    return DateofBirth.CalculateAge();
         //}
