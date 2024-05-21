5ad1feff515c74ab03818a78293e191b17b3fe9c
k3v-5 <142853881+k3v-5@users.noreply.github.com>
2024-05-20 21:18:43 -0600

    Created mesages controller, updated message repository and added message params

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -2,16 +2,20 @@ using API.DTOs;
 using API.Entities;
 using API.Helpers;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 
 namespace API.Data;
 
 public class MessageRepository : IMessageRepository
 {
     private readonly DataContext _context;
+      private readonly IMapper _mapper;
 
-    public MessageRepository(DataContext context)
+     public MessageRepository(DataContext context, IMapper mapper)
     {
         _context = context;
+        _mapper = mapper;
     }
 
     public void AddMessage(Message message)
@@ -29,10 +33,22 @@ public class MessageRepository : IMessageRepository
         return await _context.Messages.FindAsync(id);
     }
 
-    public Task<PagedList<MessageDto>> GetMessagesForUserAsync()
-    {
-        throw new NotImplementedException();
-    }
+  public async Task<PagedList<MessageDto>> GetMessagesForUserAsync(MessageParams mp)    {
+ var query = _context.Messages
+            .OrderByDescending(m => m.MessageSent)
+            .AsQueryable();
+
+        query = mp.Container.ToLowerInvariant() switch
+        {
+            "inbox" => query.Where(u => u.RecipientUsername == mp.Username),
+            "outbox" => query.Where(u => u.SenderUsername == mp.Username),
+            _ => query.Where(u => u.RecipientUsername == mp.Username && u.DateRead == null)
+        };
+
+        var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
+
+        return await PagedList<MessageDto>
+            .CreateAsync(messages, mp.PageNumber, mp.PageSize);    }
 
     public Task<IEnumerable<MessageDto>> GetMessageThreadAsync(int currentUserId, int recipientId)
     {
--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -9,7 +9,7 @@ public interface IMessageRepository
     void AddMessage(Message message);
     void DeleteMessage(Message message);
     Task<Message> GetMessageAsync(int id);
-    Task<PagedList<MessageDto>> GetMessagesForUserAsync();
+    Task<PagedList<MessageDto>> GetMessagesForUserAsync(MessageParams messageParams);
     Task<IEnumerable<MessageDto>> GetMessageThreadAsync(int currentUserId, int recipientId);
     Task<bool> SaveAllAsync();
 }
