8b31d090295274f33bd6d5e9ee525492f0fb69c6
k3v-5 <142853881+k3v-5@users.noreply.github.com>
2024-05-20 20:36:36 -0600

    Updated the list component

--- SPA/src/app/_services/members.service.ts
+++ SPA/src/app/_services/members.service.ts
@@ -95,8 +95,10 @@ export class MembersService {
     return this.http.post(this.baseUrl + "likes/" + username, {});
   }
 
-  getLikes(predicate: string): Observable<Object> {
-    return this.http.get(this.baseUrl + "likes?predicate=" + predicate);
+  getLikes(predicate: string): Observable<Member[]> {
+    return this.http.get<Member[]>(
+      this.baseUrl + "likes?predicate=" + predicate
+    );
   }
 
   deletePhoto(photoId: number): Observable<Object> {
