599c3297922a2e1c2109f972880a46a48fea0cca
k3v-5 <142853881+k3v-5@users.noreply.github.com>
2024-05-21 11:33:43 -0600

    Added the message thread to SPA

--- SPA/src/app/_services/message.service.ts
+++ SPA/src/app/_services/message.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
 import { getPaginatedResult, getPaginationHeaders } from "./paginationHelper";
 import { Message } from "../_models/message";
+import { Member } from "../_models/member";
+import { Observable } from "rxjs";
+import { PaginatedResult } from "../_models/pagination";
 
 @Injectable({
   providedIn: "root",
@@ -12,13 +15,23 @@ export class MessageService {
 
   constructor(private http: HttpClient) {}
 
-  getMessages(pageNumber: number, pageSize: number, container: string) {
+  getMessages(
+    pageNumber: number,
+    pageSize: number,
+    container: string
+  ): Observable<PaginatedResult<Message[]>> {
     let params = getPaginationHeaders(pageNumber, pageSize);
     params = params.append("Container", container);
     return getPaginatedResult<Message[]>(
       this.baseUrl + "messages",
       params,
       this.http
+    );
+  }
+
+  getMessageThread(username: string): Observable<Message[]> {
+    return this.http.get<Message[]>(
+      this.baseUrl + "messages/thread/" + username
     );
   }
 }
