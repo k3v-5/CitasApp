8cd67f98266444064392028e95cc0e20113f4dce
k3v-5 <142853881+k3v-5@users.noreply.github.com>
2024-05-30 10:56:11 -0600

    Added send message feature

--- SPA/src/app/members/member-messages/member-messages.component.ts
+++ SPA/src/app/members/member-messages/member-messages.component.ts
@@ -1,16 +1,36 @@
 import { CommonModule } from "@angular/common";
 import { Message } from "src/app/_models/message";
 import { TimeagoModule } from "ngx-timeago";
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit, ViewChild } from "@angular/core";
+import { FormsModule, NgForm } from "@angular/forms";
+import { MessageService } from "src/app/_services/message.service";
 
 @Component({
   selector: "app-member-messages",
   standalone: true,
   templateUrl: "./member-messages.component.html",
   styleUrls: ["./member-messages.component.css"],
-  imports: [CommonModule, TimeagoModule],
+  imports: [CommonModule, TimeagoModule, FormsModule],
 })
-export class MemberMessagesComponent {
+export class MemberMessagesComponent implements OnInit {
+  @ViewChild("messageForm") messageForm?: NgForm;
   @Input() username?: string;
   @Input() messages: Message[] = [];
+  messageContent = "";
+
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit(): void {}
+
+  sendMessage(): void {
+    if (!this.username) return;
+    this.messageService
+      .sendMessage(this.username, this.messageContent)
+      .subscribe({
+        next: (message) => {
+          this.messages.push(message);
+          this.messageForm?.reset();
+        },
+      });
+  }
 }
