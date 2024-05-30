9f3df82c6cfa0f7d0cc0e58eeaeacd68b66c215b
k3v-5 <142853881+k3v-5@users.noreply.github.com>
2024-05-30 10:45:30 -0600

    Updated message tab to load messages when activitated

--- SPA/src/app/members/member-detail/member-detail.component.ts
+++ SPA/src/app/members/member-detail/member-detail.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from "@angular/common";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { TabsModule } from "ngx-bootstrap/tabs";
+import { TabDirective, TabsModule, TabsetComponent } from "ngx-bootstrap/tabs";
 import { Member } from "src/app/_models/member";
 import { MembersService } from "src/app/_services/members.service";
 import { GalleryItem, GalleryModule, ImageItem } from "ng-gallery";
 import { TimeagoModule } from "ngx-timeago";
 import { MemberMessagesComponent } from "../member-messages/member-messages.component";
+import { MessageService } from "src/app/_services/message.service";
+import { Message } from "src/app/_models/message";
 
 @Component({
   selector: "app-member-detail",
@@ -22,16 +24,35 @@ import { MemberMessagesComponent } from "../member-messages/member-messages.comp
   ],
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild("memberTabs") memberTabs?: TabsetComponent;
   member: Member | undefined;
   images: GalleryItem[] = [];
+  activeTab?: TabDirective;
+  messages: Message[] = [];
 
   constructor(
     private membersService: MembersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private messageService: MessageService
   ) {}
 
   ngOnInit(): void {
     this.loadMember();
+  }
+
+  onTabActivated(data: TabDirective) {
+    this.activeTab = data;
+    if (this.activeTab.heading === "Mensajes") {
+      this.loadMessages();
+    }
+  }
+
+  loadMessages() {
+    if (this.member) {
+      this.messageService.getMessageThread(this.member.userName).subscribe({
+        next: (messages) => (this.messages = messages),
+      });
+    }
   }
 
   loadMember() {
--- SPA/src/app/members/member-messages/member-messages.component.ts
+++ SPA/src/app/members/member-messages/member-messages.component.ts
@@ -1,30 +1,16 @@
 import { CommonModule } from "@angular/common";
-import { Component, Input, OnInit } from "@angular/core";
 import { Message } from "src/app/_models/message";
-import { MessageService } from "src/app/_services/message.service";
+import { TimeagoModule } from "ngx-timeago";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-member-messages",
   standalone: true,
   templateUrl: "./member-messages.component.html",
   styleUrls: ["./member-messages.component.css"],
-  imports: [CommonModule],
+  imports: [CommonModule, TimeagoModule],
 })
-export class MemberMessagesComponent implements OnInit {
+export class MemberMessagesComponent {
   @Input() username?: string;
-  messages: Message[] = [];
-
-  constructor(private messageService: MessageService) {}
-
-  ngOnInit(): void {
-    this.loadMessages();
-  }
-
-  loadMessages() {
-    if (this.username) {
-      this.messageService.getMessageThread(this.username).subscribe({
-        next: (messages) => (this.messages = messages),
-      });
-    }
-  }
+  @Input() messages: Message[] = [];
 }
